d14b61262d8e27fb35f5aec7d2b102a6c2cca95c
altnbsmehmet <altnbsmehmet@icloud.com>
2025-07-23 01:45:31 +0300

    transition added to navbarbutton

--- src/components/buttons/NavBarButton.js
+++ src/components/buttons/NavBarButton.js
@@ -2,7 +2,8 @@ function NavBarButton({ children, onClick, className }) {
     return (
         <button
             onClick={onClick}
-            className={`py-1 px-4 text-white rounded-full transition hover:border-white text-sm ${className}`}
+            className={`py-1 px-4 text-white rounded-full transition text-sm 
+                transition duration-250 hover:text-gray-400 ${className}`}
         >
             {children}
         </button>
