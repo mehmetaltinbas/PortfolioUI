ac0968773487bb949106a92a80f274407b63ddb0
altnbsmehmet <altnbsmehmet@icloud.com>
2025-07-28 17:09:05 +0300

    in introduction, changed text color of name, position and centered description

--- src/components/sections/visitor/Introduction.js
+++ src/components/sections/visitor/Introduction.js
@@ -1,11 +1,11 @@
 function Introduction({ user, downloadCV }) {
     return (
         <div className="flex flex-col justify-center items-center gap-2">
-            <p className="text-2xl font-bold text-center">
+            <p className="text-2xl font-bold text-center text-[#003366]">
                 {user?.firstName} {user?.lastName}
             </p>
-            <p>{user?.position}</p>
-            <p>{user?.bio}</p>
+            <p className="text-l font-semibold text-center text-[#174978]">{user?.position}</p>
+            <p className="text-center">{user?.bio}</p>
             <button
                 onClick={downloadCV}
                 className="py-1 px-4 border-[1px] border-gray rounded-full hover:border-black"
